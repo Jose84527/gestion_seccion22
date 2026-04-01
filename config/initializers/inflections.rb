6a7616eb636847bfece85b42de319153dc3c38f9
Jose84527 <josejgp845@gmail.com>
2026-04-01 12:02:19 -0600

    Fin de ña fase 1

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,16 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, "\\1en"
-#   inflect.singular /^(ox)en/i, "\\1"
-#   inflect.irregular "person", "people"
-#   inflect.uncountable %w( fish sheep )
-# end
-
-# These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular "historial", "historiales"
+  inflect.irregular "trabajador", "trabajadores"
+  inflect.irregular "usuario", "usuarios"
+  inflect.irregular "actividad", "actividades"
+  inflect.irregular "cooperacion", "cooperaciones"
+
+  inflect.irregular "asistencia_actividad", "asistencia_actividades"
+  inflect.irregular "concepto07_nivel", "concepto07_niveles"
+  inflect.irregular "cooperacion_afiliado", "cooperacion_afiliados"
+  inflect.irregular "cooperacion_concepto07_config", "cooperacion_concepto07_configs"
 end
